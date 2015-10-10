005df98f34afc307c943b38e8c85adcf72f7c780
Hack Reactor Students <accounts@hackreactor.com>
2015-10-09 21:35:56 -0700

    Comnpleted the review of the simple data structures moving onto the advanced

---
+++ sprint-two/spec/doubly_linked_lists.js
@@ -0,0 +1,62 @@
+describe('Doubly Linked Lists', function() {
+  var dlinkedList;
+
+  beforeEach(function() {
+    dlinkedList = new dLinkedList();
+  });
+
+  it('should have a head and tail', function() {
+    expect(dlinkedList).to.have.property("head");
+    expect(dlinkedList).to.have.property("tail");
+  });
+
+  it('should have methods named "addToTail", "removeHead", and "contains"', function() {
+    expect(dlinkedList.addToTail).to.be.a("function");
+    expect(dlinkedList.removeHead).to.be.a("function");
+    expect(dlinkedList.contains).to.be.a("function");
+  });
+
+  it('nodes should have a ".previous" property', function(){
+    dlinkedList.addToTail(4);
+    expect(dlinkedList).to.have.property(previous);
+    dlinkedList.addToTail(5);
+    expect(dlinkedList.tail.value).to.equal(5);
+  });
+
+  it('should designate a new tail when new nodes are added', function(){
+    dlinkedList.addToTail(4);
+    expect(dlinkedList.tail.value).to.equal(4);
+    dlinkedList.addToTail(5);
+    expect(dlinkedList.tail.value).to.equal(5);
+  });
+
+  it('should remove the head from the list when removeHead is called', function(){
+    dlinkedList.addToTail(4);
+    dlinkedList.addToTail(5);
+    expect(dlinkedList.head.value).to.equal(4);
+    dlinkedList.removeHead();
+    expect(dlinkedList.head.value).to.equal(5);
+  });
+
+  it("should return the value of the former head when removeHead is called", function(){
+    dlinkedList.addToTail(4);
+    expect(dlinkedList.removeHead()).to.equal(4);
+  });
+
+  it("should contain a value that was added", function(){
+    dlinkedList.addToTail(4);
+    dlinkedList.addToTail(5);
+    expect(dlinkedList.contains(4)).to.equal(true);
+    expect(dlinkedList.contains(5)).to.equal(true);
+    expect(dlinkedList.contains(6)).to.equal(false);
+  });
+
+  it('should not contain a value that was removed', function(){
+    dlinkedList.addToTail(4);
+    dlinkedList.addToTail(5);
+    dlinkedList.removeHead();
+    expect(dlinkedList.contains(4)).to.equal(false);
+  });
+
+  // add more tests here to test the functionality of dlinkedList
+});
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,55 @@
 var BinarySearchTree = function(value) {
+  this.left = null
+  this.right = null
+  this.value = value;
 
-
+  this.insert = function(val){
+    if(val > this.value){
+        if(!this.right){
+          this.right = new BinarySearchTree(val);
+        }else{
+          this.right.insert(val);
+        }
+    }else{
+      if(!this.left){
+        this.left = new BinarySearchTree(val)
+      }else{
+        this.left.insert(val);
+      }
+    }
+  };
+  this.contains = function(val){
+    if(this.value === val){
+      return true;
+    }
+    if(val > this.value){
+        if(!this.right){
+          return false;
+        }else{
+          return this.right.contains(val);
+        }
+    }else{
+      if(!this.left){
+        return false;
+      }else{
+        return this.left.contains(val);
+      }
+    }
+  };
+  this.depthFirstLog = function(cb){
+    // debugger;
+    function trav(node){
+      cb(node.value);
+      if(node.right){
+        return trav(node.right);
+      }
+      if(node.left){
+        return trav(node.left);
+      }
+      return;
+    }
+    return trav(this);
+  };
 
 };
 
